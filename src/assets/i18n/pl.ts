4e81bc4a784f150c95119cd01651477edefa5ce6
Kamil Mossakowski <kamil.mossakowski@emphie.com>
2023-11-12 00:18:29 +0100

    init tailwind, route and translation

---
+++ src/assets/i18n/pl.ts
@@ -0,0 +1,7 @@
+export const messages = {
+  pl: {
+    message: {
+      hello: "siemano",
+    },
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { createI18n } from "vue-i18n";
+import "./index.css";
+import { messages } from "./assets/i18n/pl";
 
-createApp(App).use(router).mount("#app");
+const i18n = createI18n({
+  locale: "pl",
+  messages,
+});
+
+createApp(App).use(router).use(i18n).mount("#app");
